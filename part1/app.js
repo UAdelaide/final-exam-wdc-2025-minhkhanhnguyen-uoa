4b1537c0ac1faa93e3b58f1ab28f76971323239b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 03:47:10 +0000

    [automatic commit] 2025-06-20 03:47:10 UTC 129.127.202.12

--- part1/app.js
+++ part1/app.js
@@ -31,7 +31,7 @@ let db;
 
         // Insert data if not exist
         // for Users table
-        const [user_rows] = await db.query(``)
+        const [user_rows] = await db.execute(`SELECT COUNT(*) FROM Users`)
 
 
 
