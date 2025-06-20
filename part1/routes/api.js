c0bade9dbfd23248410e5a7a810801f42d74a241
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:16:21 +0000

    [automatic commit] 2025-06-20 05:16:21 UTC 129.127.202.12

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -36,7 +36,7 @@ router.get('/walkers/summary', async (req, res) => {
         const [walkers] = await db.execute(`
             SELECT u.username AS walker_username,
             (SELECT COUNT(*) FROM WalkRatings AS wr JOIN Users AS u ON u.user_id = wr.walker_id WHERE u.username = walker_username) AS total_ratings,
-
+            AVG()
         `);
     } catch (err) {
         console.log('Error in api/walkers/summary: ', err);
