8c1e00832501c9a05a84cf35e2541d413b0ea906
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:47:35 +0000

    [automatic commit] 2025-06-20 14:47:34 UTC 122.150.228.168

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -29,7 +29,7 @@ router.post('/', async (req, res) => {
   try {
     const [result] = await db.query(`
       INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location)
-      VALUES (, ?, ?, ?)
+      VALUES ((SELECT dog_id FROM Dogs WHERE ), ?, ?, ?)
     `, [dog_name, requested_time, duration_minutes, location]);
 
     res.status(201).json({ message: 'Walk request created', request_id: result.insertId });
