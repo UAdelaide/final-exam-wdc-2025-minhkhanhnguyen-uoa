88891cdc200e14af9451a761aa2cb3b9633396a2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:19:09 +0000

    [automatic commit] 2025-06-20 04:19:09 UTC 129.127.202.12

--- starthere/routes/api.js
+++ starthere/routes/api.js
@@ -4,9 +4,9 @@ var router = express.Router();
 module.exports = (db) => {
     router.get('/dogs', async (req, res) => {
         try {
-
+            const [rows]
         } catch (err) {
-            
+            res.status(500).json({ error: 'Failed to fetch dogs' });
         }
     });
 
