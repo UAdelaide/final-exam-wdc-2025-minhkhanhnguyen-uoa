6b976ab7ff0a6c0219f95817e563962618bfef0b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:44:30 +0000

    [automatic commit] 2025-06-20 04:44:30 UTC 129.127.202.12

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var { init_db } = require('../db.js');
 
-const db = await init_db();
+
 
 router.get('/dogs', async (req, res) => {
     try {
-
+        const db = await init_db();
         const [dogs] = await db.execute(`SELECT * FROM Dogs`);
         res.json(dogs);
     } catch (err) {
