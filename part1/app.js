4b664e886b55d201d5357ae83f23377f99a53114
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:11:26 +0000

    [automatic commit] 2025-06-20 04:11:26 UTC 129.127.202.12

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,7 @@ var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var 
+var apiRouter = require('./routes/api');
 
 var app = express();
 
