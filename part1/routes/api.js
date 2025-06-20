cefdf90b6b62bd0de3301a93c58fdf2144bebf89
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:58:06 +0000

    [automatic commit] 2025-06-20 04:58:06 UTC 129.127.202.12

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -17,7 +17,7 @@ router.get('/walkrequests/open', async (req, res) => {
     try {
         const db = await init_db();
         const [open_wlkreq] = db.execute(`
-            SELECT r.request_id, d.name AS dog_name, r.requested_time, r.duration_minutes, 
+            SELECT r.request_id, d.name AS dog_name, r.requested_time, r.duration_minutes, r.location, u.username AS on
         `);
         res.json(open_wlkreq);
     } catch (err) {
